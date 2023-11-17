733b224aa29edf99de8096662732b96cfbd3c92d
Pranav <101118335+pranavchachra1702@users.noreply.github.com>
2023-11-18 02:34:34 +0530

    Add files via upload

---
+++ wallc.sh
@@ -0,0 +1,38 @@
+#!/bin/bash/
+echo '
+Script for making a .xml file for gnome wallpapers (42+)
+'
+name=$(zenity --entry --text "Please enter file name");
+
+#Light_wp=$(zenity --file-selection --filename <location> --title="Pick Light Wallpaper")
+#Dark_wp=$(zenity --file-selection --filename <location> --title="Pick Dark Wallpaper")
+
+# Un-comment lines above  (7, 8) if you want to specify the default directory for images, replace <location>
+# with dir path and don't forget to comment line number 13 and 14
+
+Light_wp=$(zenity --file-selection --title="Pick Light Wallpaper")
+Dark_wp=$(zenity --file-selection --title="Pick Dark Wallpaper")
+
+touch $name.xml
+
+content0='<?xml version="1.0"?>
+<!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">
+<wallpapers>
+ <wallpaper deleted="false">'
+content1="  <name>${name}</name>
+   <filename>${Light_wp}</filename>
+   <filename-dark>${Dark_wp}</filename-dark>
+   <options>zoom</options>
+   <shade_type>solid</shade_type>
+   <pcolor>#3465a4</pcolor>
+   <scolor>#000000</scolor>
+ </wallpaper>
+</wallpapers>"
+ 
+echo "" > $name.xml 
+echo "$content0" > $name.xml
+echo "$content1" >> $name.xml
+
+sudo <<< "echo $(zenity --password --title "Authentication")" mv $name.xml /usr/share/gnome-background-properties/ 
+
+zenity --notification --text "New Wallpaper has been Added!"
